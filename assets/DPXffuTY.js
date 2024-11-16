d9552ad6e4ee016223d0e2dd28b493eb2d2ab245
Laixander Naguit <laixander@gmail.com>
2024-11-16 23:57:00 +0800

    Updates

---
+++ assets/DPXffuTY.js
@@ -0,0 +1,1 @@
+import{_ as t,t as o,v as r,x as s}from"./Cow688mB.js";const n={};function l(a,e){return o(),r("div",null,e[0]||(e[0]=[s("h1",{class:"text-red-500 text-2xl font-bold custom"},"Hello World from Pages!",-1)]))}const d=t(n,[["render",l]]);export{d as default};
