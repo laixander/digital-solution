b7ba76d36c37da74325e78099e807a8877e59639
Laixander Naguit <laixander@gmail.com>
2024-11-16 23:44:47 +0800

    Updates

---
+++ assets/CY8fCrEN.js
@@ -0,0 +1,1 @@
+import{_ as t,t as o,v as r,x as s}from"./CjAGp5rk.js";const n={};function l(a,e){return o(),r("div",null,e[0]||(e[0]=[s("h1",{class:"text-red-500 text-2xl font-bold"},"Hello World from Pages!",-1)]))}const d=t(n,[["render",l]]);export{d as default};
--- assets/index.8dT0NZyR.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,v as o,t as r}from"./entry._lZQUs5k.js";const c={};function n(t,s){return r(),o("div",null," Hello World from Pages! ")}const l=e(c,[["render",n]]);export{l as default};
