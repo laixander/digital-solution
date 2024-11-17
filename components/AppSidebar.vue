302a89cbde7acd7694be722a311975601ce2cced
Laixander Naguit <laixander@gmail.com>
2024-11-17 23:26:51 +0800

    Updates

--- components/AppSidebar.vue
+++ components/AppSidebar.vue
@@ -261,23 +261,23 @@
                             <div class="p-1.5 space-y-0.5">
                                 <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
                                     href="#">
-                                    <Bell class="shrink-0 size-4" />
+                                    <LayoutGrid class="shrink-0 size-4" />
+                                    App Portal
+                                </a>
+                                <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
+                                    href="#">
+                                    <Newspaper class="shrink-0 size-4" />
                                     Newsletter
                                 </a>
                                 <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
                                     href="#">
-                                    <ShoppingBag class="shrink-0 size-4" />
-                                    Purchases
+                                    <Bolt class="shrink-0 size-4" />
+                                    Settings
                                 </a>
                                 <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
                                     href="#">
-                                    <CloudDownload class="shrink-0 size-4" />
-                                    Downloads
-                                </a>
-                                <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
-                                    href="#">
-                                    <Users class="shrink-0 size-4" />
-                                    Team Account
+                                    <LogOut class="shrink-0 size-4" />
+                                    Logout
                                 </a>
                             </div>
                         </div>
@@ -299,5 +299,5 @@
 </template>
 
 <script setup>
-import { ChevronDown, ChevronUp, House, Users, UserCog, Briefcase, CalendarDays, BookOpen, Bell, ShoppingBag, CloudDownload } from 'lucide-vue-next';
+import { ChevronDown, ChevronUp, House, Users, UserCog, Briefcase, CalendarDays, BookOpen, Bell, LayoutGrid, Bolt, Newspaper, LogOut } from 'lucide-vue-next';
 </script>
