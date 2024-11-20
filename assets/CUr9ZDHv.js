41e8a5dff609c6f9ab3b353d82f1f2b9490a7117
Laixander Naguit <laixander@gmail.com>
2024-11-20 16:55:22 +0800

    Updates

---
+++ assets/CUr9ZDHv.js
@@ -0,0 +1,1 @@
+import{u as s}from"./C0DKsdHY.js";import{o as t,c as o,S as a}from"./BNtFBlB0.js";const n={__name:"simple",setup(r){return s({bodyAttrs:{class:"simple-layout"}}),(e,l)=>(t(),o("div",null,[a(e.$slots,"default")]))}};export{n as default};
