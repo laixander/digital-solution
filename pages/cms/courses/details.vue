dda76b5063d0f47786adb665b9abd800779ce75e
Laixander Naguit <laixander@gmail.com>
2024-11-25 22:51:30 +0800

    Updates

--- pages/cms/courses/details.vue
+++ pages/cms/courses/details.vue
@@ -92,7 +92,7 @@
                                 </div>
                             </div>
 
-                            <button type="button" class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-gray-200 text-gray-800 hover:bg-gray-300 focus:outline-none focus:bg-gray-200 disabled:opacity-50 disabled:pointer-events-none dark:bg-white/10 dark:text-white dark:hover:bg-white/20 dark:hover:text-white dark:focus:bg-white/20 dark:focus:text-white">
+                            <button type="button" class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700">
                                 <PenLineIcon class="shrink-0 size-4" />
                                 Edit
                             </button>
