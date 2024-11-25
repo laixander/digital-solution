61901a0ab62eb70e275d1ca18a9a1abb30fa7630
Laixander Naguit <laixander@gmail.com>
2024-11-25 17:01:39 +0800

    Updates

--- assets/DUAT2L0X.js
+++
@@ -1,1 +0,0 @@
-import{u as s}from"./BO9gOKnk.js";import{o as t,c as o,E as a}from"./CR8WtNni.js";const n={__name:"simple",setup(r){return s({bodyAttrs:{class:"simple-layout"}}),(e,l)=>(t(),o("div",null,[a(e.$slots,"default")]))}};export{n as default};
