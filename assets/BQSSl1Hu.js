50800bc1584c2df278c40d08dec8372bd8ec6f5c
Laixander Naguit <laixander@gmail.com>
2024-11-26 17:02:47 +0800

    Updates

---
+++ assets/BQSSl1Hu.js
@@ -0,0 +1,1 @@
+import{u as s}from"./Z-2vhvww.js";import{o as a,c as t,r as o}from"./CwrJspsG.js";const p={__name:"special",setup(r){return s({bodyAttrs:{class:"special-layout"}}),(e,l)=>(a(),t("div",null,[o(e.$slots,"default")]))}};export{p as default};
