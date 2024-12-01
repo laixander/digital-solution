8b6412379706d6f9bb6fe0b8d64403fe1d39f4dc
Laixander Naguit <laixander@gmail.com>
2024-12-01 22:31:35 +0800

    Updates

--- assets/BvWYLlza.js
+++
@@ -1,1 +0,0 @@
-import{u as s}from"./OiYjXJie.js";import{o as t,c as o,r}from"./BujVE5Lh.js";const n={__name:"simple",setup(a){return s({bodyAttrs:{class:"simple-layout"}}),(e,l)=>(t(),o("div",null,[r(e.$slots,"default")]))}};export{n as default};
--- assets/C6CWJuTx.js
+++
@@ -1,1 +0,0 @@
-import{p}from"./BujVE5Lh.js";const o=p("/img/placeholder.jpg");export{o as _};
