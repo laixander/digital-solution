f3b151fe22d29283ca6b5edb21f1fabd00494125
Laixander Naguit <laixander@gmail.com>
2024-12-02 16:57:45 +0800

    Updates

---
+++ assets/S4KxVGTN.js
@@ -0,0 +1,1 @@
+import{u as s}from"./_AwPrdz1.js";import{o as a,c as t,g as o}from"./BGZCah1A.js";const p={__name:"special",setup(r){return s({bodyAttrs:{class:"special-layout"}}),(e,l)=>(a(),t("div",null,[o(e.$slots,"default")]))}};export{p as default};
---
+++ assets/sqQMN8eS.js
@@ -0,0 +1,1 @@
+import{u as s}from"./_AwPrdz1.js";import{o as t,c as o,g as a}from"./BGZCah1A.js";const n={__name:"simple",setup(r){return s({bodyAttrs:{class:"simple-layout"}}),(e,l)=>(t(),o("div",null,[a(e.$slots,"default")]))}};export{n as default};
