c0475bf9250090f9ed6a74b4ef21e3d09b3a6883
Laixander Naguit <laixander@gmail.com>
2024-12-03 17:46:58 +0800

    Updates

---
+++ assets/CporgWWE.js
@@ -0,0 +1,1 @@
+import{u as s}from"./aek-f1jZ.js";import{o as t,c as o,h as a}from"./yJjsSqZi.js";const n={__name:"simple",setup(r){return s({bodyAttrs:{class:"simple-layout"}}),(e,l)=>(t(),o("div",null,[a(e.$slots,"default")]))}};export{n as default};
---
+++ assets/N3vd9ChV.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as r}from"./yJjsSqZi.js";const c={};function n(s,t){return o(),r("div",null," Hello World ")}const _=e(c,[["render",n]]);export{_ as default};
