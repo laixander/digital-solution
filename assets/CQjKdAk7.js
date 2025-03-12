e8d0b3f8cbd2193bb9ab36f43978b82ce3f4a155
Laixander Naguit <laixander@gmail.com>
2025-03-12 11:22:05 +0800

    Updates

---
+++ assets/CQjKdAk7.js
@@ -0,0 +1,1 @@
+import{u as s}from"./PsMAaiSn.js";import{o as t,c as o,h as a}from"./Fc47KF3C.js";const n={__name:"simple",setup(r){return s({bodyAttrs:{class:"simple-layout"}}),(e,l)=>(t(),o("div",null,[a(e.$slots,"default")]))}};export{n as default};
---
+++ assets/j4TM-EkK.js
@@ -0,0 +1,1 @@
+import{u as s}from"./PsMAaiSn.js";import{o as a,c as t,h as o}from"./Fc47KF3C.js";const p={__name:"special",setup(r){return s({bodyAttrs:{class:"special-layout"}}),(e,l)=>(a(),t("div",null,[o(e.$slots,"default")]))}};export{p as default};
